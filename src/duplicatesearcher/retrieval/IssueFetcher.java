f5df851c6517966d665d3aa503071f29600e7f48
Anton Österberg <anton.osterberg@gmail.com>
2016-03-19 19:47:55 +0100

    Changes to the interface IssueFetcher to make it more generic.

--- src/duplicatesearcher/retrieval/IssueFetcher.java
+++ src/duplicatesearcher/retrieval/IssueFetcher.java
@@ -1,13 +1,16 @@
 package duplicatesearcher.retrieval;
 
-import java.util.Set;
-
+import java.io.IOException;
+import java.util.Collection;
 import org.eclipse.egit.github.core.Issue;
 
 public interface IssueFetcher
 {
-	Set<Issue> getOpenIssues(int amount);
-	Set<Issue> getOpenIssues();
-	Set<Issue> getClosedIssues(int amount);
-	Set<Issue> getClosedIssues();
+	Collection<Issue> getOpenIssues(int amount) throws IOException;
+
+	Collection<Issue> getOpenIssues() throws IOException;
+
+	Collection<Issue> getClosedIssues(int amount) throws IOException;
+
+	Collection<Issue> getClosedIssues() throws IOException;
 }
