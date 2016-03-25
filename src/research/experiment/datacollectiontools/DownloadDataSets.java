35ec2c1371b1abec1bd9abe768b27e62d02f24a3
Anton Österberg <anton.osterberg@gmail.com>
2016-03-25 22:39:50 +0100

    DownloadDataSets can now be run with arguments from the command line and downloads issues and comments in the same run.

--- src/research/experiment/datacollectiontools/DownloadDataSets.java
+++ src/research/experiment/datacollectiontools/DownloadDataSets.java
@@ -1,19 +1,19 @@
 package research.experiment.datacollectiontools;
 
-import static org.junit.Assert.assertEquals;
-
 import java.io.IOException;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import org.eclipse.egit.github.core.Comment;
 import org.eclipse.egit.github.core.Issue;
 import org.eclipse.egit.github.core.RepositoryId;
 import org.eclipse.egit.github.core.client.GitHubClient;
 
 import duplicatesearcher.retrieval.CommentRetriever;
 import duplicatesearcher.retrieval.GitHubTestClient;
-import duplicatesearcher.retrieval.IssueFetcher;
 import duplicatesearcher.retrieval.IssueRetriever;
 
 public class DownloadDataSets
@@ -22,25 +22,23 @@ public class DownloadDataSets
 	public static void main(String[] args) throws IOException, ClassNotFoundException
 	{
 		final GitHubClient client = new GitHubTestClient();
-		final RepositoryId repoInfo = new RepositoryId("golang", "go");
-
-//		Redan gjort!		
-//		IssueFetcher retriever = new IssueRetriever(client, repoInfo);
-//		
-//		final Collection<Issue> resultOpen = retriever.getOpenIssues();
-//		final DatasetFileManager saveOpen = new DatasetFileManager(repoInfo, resultOpen);
-//		saveOpen.save("_open");
-//		
-//		final Collection<Issue> resultClosed = retriever.getClosedIssues();
-//		final DatasetFileManager saveClosed = new DatasetFileManager(repoInfo, resultClosed);
-//		saveClosed.save("_closed");
+		final RepositoryId repoInfo = new RepositoryId(args[0], args[1]);
 		
-		final DatasetFileManager fileClosedIssue = new DatasetFileManager(repoInfo);
-		fileClosedIssue.load("_closed");
-		Set<Issue> closedIssues = new HashSet(fileClosedIssue.getDataset());
-		CommentRetriever commentRetriever = new CommentRetriever(client, repoInfo, closedIssues);
+		IssueRetriever retriever = new IssueRetriever(client, repoInfo);
+		Collection<Issue> open = retriever.getOpenIssues();
+		Collection<Issue> closed = retriever.getClosedIssues();
 		
-		final DatasetFileManager
+		Set<Issue> allIssues = new HashSet<Issue>(closed);
+		allIssues.addAll(open);
+		System.out.println(allIssues.size());
+		
+		CommentRetriever commentRetriever = new CommentRetriever(client, repoInfo, allIssues);
+		commentRetriever.findIssuesWithComments();
+		commentRetriever.downloadComments();
+		Map<Issue, List<Comment>> issuesAndComments = commentRetriever.getIssuesAndComments();
+		
+		DatasetFileManager files = new DatasetFileManager(repoInfo, issuesAndComments);
+		files.save();
 	}
 
 }
