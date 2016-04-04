456a6a67f48bdd588b280c917ccb5ea8e5353971
Anton Österberg <anton.osterberg@gmail.com>
2016-04-04 20:07:26 +0200

    Refactoring of InverseDocumentFrequencyCounter, should be working as intended at this stage, but test cases has not been rewritten to confirm this yet.

--- src/duplicatesearcher/analysis/frequency/InverseDocumentFrequencyCounter.java
+++ src/duplicatesearcher/analysis/frequency/InverseDocumentFrequencyCounter.java
@@ -1,32 +1,108 @@
 package duplicatesearcher.analysis.frequency;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
-import duplicatesearcher.StrippedIssue;
 import duplicatesearcher.processing.Tokenizer;
 
-public class InverseDocumentFrequencyCounter extends FrequencyCounter
+/**
+ * The {@link InverseDocumentFrequencyCounter} measures in how many documents a
+ * word occurs. Unlike the {@link TermFrequencyCounter}, this class does not
+ * take any consideration to how many times a word occurs within each specific
+ * document, only in how many documents.
+ *
+ */
+public class InverseDocumentFrequencyCounter implements FrequencyCounter
 {
-	private final Set<Integer> documents = new HashSet<Integer>();
-	
-	public int add(final StrippedIssue input)
+	private final Map<String, Integer> frequency = new HashMap<String, Integer>();
+	private final Map<Integer, Set<String>> addedTokens = new HashMap<Integer, Set<String>>(100);
+
+	@Override
+	public int getTokenFrequency(final String token)
 	{
-		if(!documents.add(input.getNumber()))
+		if(!frequency.containsKey(token))
 			return 0;
-		
-		final Set<String> tokens = input.wordSet();
-		for(String str : tokens)
-			increment(str);
-		
-		return tokens.size();
+
+		return frequency.get(token);
 	}
-	
+
+	private boolean increment(String token)
+	{
+		if(token == null || token.length() == 0)
+			return false;
+		if(!frequency.containsKey(token))
+		{
+			frequency.put(token, 1);
+		}
+		else
+		{
+			final int tokenFrequency = frequency.get(token) + 1;
+			frequency.put(token, tokenFrequency);
+		}
+		return true;
+	}
+
+	public int add(final int id, final Set<String> input)
+	{
+		Set<String> tokens = tokenizeInput(input);
+		return addTokens(id, tokens);
+	}
+
+	private Set<String> tokenizeInput(Set<String> input)
+	{
+		final Set<String> tokens = new HashSet<String>();
+
+		for(String str : input)
+		{
+			if(Tokenizer.isToken(str))
+			{
+				tokens.add(str);
+			}
+			else
+			{
+				Tokenizer tokenizer = new Tokenizer(str);
+				tokens.addAll(Arrays.asList(tokenizer.getTokens()));
+			}
+
+		}
+		return tokens;
+	}
+
+	private int addTokens(int id, Set<String> tokens)
+	{
+		Set<String> savedTokens;
+		int added = 0;
+		
+		if(addedTokens.containsKey(id))
+		{
+			savedTokens = addedTokens.get(id);
+			tokens.removeAll(savedTokens);
+		}
+		else
+		{
+			savedTokens = new HashSet<String>(tokens.size());
+			addedTokens.put(id, savedTokens);
+		}
+		
+		for(String token : tokens)
+		{
+			if(increment(token))
+			{
+				savedTokens.add(token);
+				added++;
+			}
+		}
+		
+		return added;
+	}
+
 	@Override
 	public double getWeight(final String token)
 	{
-		final double inverseFrequency = documents.size()/(double)getTokenFrequency(token);
-		return 1+ Math.log10(inverseFrequency);
+		final double inverseFrequency = addedTokens.size() / (double) getTokenFrequency(token);
+		return 1 + Math.log10(inverseFrequency);
 	}
 }
