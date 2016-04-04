46409e932101ce90e707f939604b209e5748e407
Anton Österberg <anton.osterberg@gmail.com>
2016-04-04 22:10:58 +0200

    Created minor test class for class Token.

---
+++ test/duplicatesearcher/TokenTest.java
@@ -0,0 +1,41 @@
+package duplicatesearcher;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TokenTest
+{
+	private final static Token ONE = new Token("one");
+	private final static Token TWO = new Token("two");
+	private final static Token THREE = new Token("three");
+
+	@Test
+	public void testHashCode()
+	{
+		Token token1 = new Token("one");
+		Token token2 = new Token("two");
+		
+		assertFalse(token1.hashCode() == token2.hashCode());
+		
+		Token token1_ = new Token("one");
+		
+		assertEquals(token1.hashCode(), token1_.hashCode());
+		
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void testTokenWithBadInput()
+	{
+		Token token = new Token("BAD");
+	}
+
+	@Test
+	public void testEqualsObject()
+	{
+		assertTrue(ONE.equals(new Token("one")));
+		assertFalse(ONE.equals(TWO));
+	}
+
+}
