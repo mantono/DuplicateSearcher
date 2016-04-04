6d990d96b3f3e06854dbad3168aae32079cf6b7f
Anton Österberg <anton.osterberg@gmail.com>
2016-04-04 21:58:20 +0200

    FrequencyCounter is now an interface rather than an abstract class. "Composition over inheritance" gives more freedom.

--- src/duplicatesearcher/analysis/frequency/FrequencyCounter.java
+++ src/duplicatesearcher/analysis/frequency/FrequencyCounter.java
@@ -1,77 +1,15 @@
 package duplicatesearcher.analysis.frequency;
 
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+
+import duplicatesearcher.Token;
 
 /**
- * This abstract class counts the occurences of a String token in an issue.
+ * This abstract class counts the occurrences of a String token in an issue.
  *
  */
-public abstract class FrequencyCounter implements Serializable
-{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -7141714544833012285L;
-	private final Map<String, Integer> frequency = new HashMap<String, Integer>();
-	
-	public abstract double getWeight(final String token);
-	
-	public int getTokenFrequency(final String token)
-	{
-		if(!frequency.containsKey(token))
-			return 0;
-		
-		return frequency.get(token);
-	}
-	
-	public Set<String> getTokens()
-	{
-		return frequency.keySet();
-	}
-	
-	public int size()
-	{
-		return frequency.size();
-	}
-	
-	public void clear()
-	{
-		frequency.clear();
-	}
-	
-	public int remove(final String key)
-	{
-		return frequency.remove(key);
-	}
-	
-	public boolean change(final String currentKey, final String newKey)
-	{
-		if(!frequency.containsKey(currentKey))
-			return false;
-		
-		final int value = frequency.get(currentKey);
-		frequency.remove(currentKey);
-		frequency.put(newKey, value);
-		return true;
-	}
-
-	protected int increment(String token)
-	{
-		if(token == null || token.length() == 0)
-			return 0;
-		if(!frequency.containsKey(token))
-		{
-			frequency.put(token, 1);
-			return 1;
-		}
-		else
-		{
-			final int tokenFrequency = frequency.get(token) + 1;
-			frequency.put(token, tokenFrequency);
-			return tokenFrequency;
-		}
-	}
+public interface FrequencyCounter extends Serializable
+{	
+	int getTokenFrequency(final Token token);
+	double getWeight(final Token token);
 }
