d049fab2064a6d79da099e240d4f27c7f797cf7f
Anton Österberg <anton.osterberg@gmail.com>
2016-04-08 18:57:35 +0200

    Updated the class IssueRetriever to be more resilient, either waiting for new requests or saving the currently downloaded issues and comments (hopefully...).

--- src/duplicatesearcher/retrieval/IssueRetriever.java
+++ src/duplicatesearcher/retrieval/IssueRetriever.java
@@ -15,30 +15,42 @@ import org.eclipse.egit.github.core.Issue;
 import org.eclipse.egit.github.core.RepositoryId;
 import org.eclipse.egit.github.core.client.GitHubClient;
 import org.eclipse.egit.github.core.client.PageIterator;
+import org.eclipse.egit.github.core.client.RequestException;
 import org.eclipse.egit.github.core.service.IssueService;
 
 public class IssueRetriever extends GitHubTask
 {
 	private final IssueService service;
 	private final Map<String, String> filter = new HashMap<String, String>();
+	private final Map<Issue, List<Comment>> issues;
 	private final RepositoryId repo;
 	private final int issueCount;
 	private int iterations = 0;
-	
+
 	public IssueRetriever(final GitHubClient client, final RepositoryId repo)
 	{
 		super(client);
 		this.service = new IssueService(client);
 		this.repo = repo;
 		this.issueCount = getAmountOfIssues();
+		issues = new HashMap<Issue, List<Comment>>(issueCount);
 	}
 	
+	public IssueRetriever(final GitHubClient client, final RepositoryId repo, Map<Issue, List<Comment>> issues)
+	{
+		super(client);
+		this.service = new IssueService(client);
+		this.repo = repo;
+		this.issueCount = getAmountOfIssues();
+		this.issues = issues;
+	}
+
 	private int getAmountOfIssues()
 	{
 		filter.put("state", "all");
 		filter.put("sort", "created");
 		filter.put("direction", "desc");
-		
+
 		final PageIterator<Issue> issuePages = service.pageIssues(repo.getOwner(), repo.getName(), filter);
 		Collection<Issue> issues = issuePages.next();
 		return getNumberOfHighestId(issues);
@@ -47,40 +59,73 @@ public class IssueRetriever extends GitHubTask
 	private int getNumberOfHighestId(Collection<Issue> issues)
 	{
 		int id = -1;
-		
-		for(Issue issue: issues)
+
+		for(Issue issue : issues)
 			if(issue.getNumber() > id)
 				id = issue.getNumber();
-		
+
 		return id;
 	}
 
-	public Map<Issue, List<Comment>> getIssues() throws IOException
+	public Map<Issue, List<Comment>> getIssues()
 	{
 		System.out.println(repo + " contains " + issueCount + " issues and pull requests");
 		filter.put("direction", "asc");
-		Map<Issue, List<Comment>> issues = new HashMap<Issue, List<Comment>>(issueCount);
 		List<Issue> issuesToProcess = new ArrayList<Issue>(30);
 		
-		int iterations = 0;
-		final PageIterator<Issue> issuePages = service.pageIssues(repo.getOwner(), repo.getName(), filter);
-		while(issuePages.hasNext())
+		final PageIterator<Issue> issuePages = getPageIterator();
+
+
+		try
 		{
-			printProgress(issues.size(), issueCount);
-			printStats();
-			
-			issuesToProcess.addAll(issuePages.next());
-			removePullRequests(issuesToProcess);
-			issues.putAll(downloadComments(issuesToProcess));
-			issuesToProcess.clear();
-			
-			sleep();
+			while(issuePages.hasNext())
+			{
+				printProgress(issues.size(), issueCount);
+				printStats();
+
+				issuesToProcess.addAll(issuePages.next());
+				removePullRequests(issuesToProcess);
+				issues.putAll(downloadComments(issuesToProcess));
+				issuesToProcess.clear();
+
+				sleep();
+			}
 		}
-		
-		
+		catch(Exception e)
+		{
+			System.err.println("Download ended prematurely.");
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+			System.err.println("Downloade data will be saved for continued retireval.");
+			return issues;
+		}
+
 		return issues;
 	}
-	
+
+	private PageIterator<Issue> getPageIterator()
+	{
+		if(!issues.isEmpty())
+		{
+			final int issuesDownloaded = getLastDownloadedIssue();
+			final int startAtPage = issuesDownloaded/100;
+			System.out.println(issuesDownloaded + " issues already downloaded, will start at page " + startAtPage);
+			return service.pageIssues(repo.getOwner(), repo.getName(), filter, 100, startAtPage);
+		}
+		
+		return service.pageIssues(repo.getOwner(), repo.getName(), filter);
+	}
+
+	private int getLastDownloadedIssue()
+	{
+		int highest = 0;
+		for(Issue issue : issues.keySet())
+			if(issue.getNumber() > highest)
+				highest = issue.getNumber();
+		
+		return highest;
+	}
+
 	private void sleep()
 	{
 		final int remainingRequests = getClient().getRemainingRequests();
@@ -94,25 +139,35 @@ public class IssueRetriever extends GitHubTask
 	private Map<Issue, List<Comment>> downloadComments(List<Issue> issuesToProcess) throws IOException
 	{
 		System.out.println("");
-		
+
 		final Map<Issue, List<Comment>> issues = new HashMap<Issue, List<Comment>>(150);
 		for(Issue issue : issuesToProcess)
 		{
-			final List<Comment> commentsFromIssue = service.getComments(repo, issue.getNumber());
-			if(commentsFromIssue == null)
-				issues.put(issue, new ArrayList<Comment>(0));
-			else
-				issues.put(issue, commentsFromIssue);
-			
-			final int remainingRequests = getClient().getRemainingRequests();
-			
-			System.out.print(".");
-			if(iterations % 10 == 0)
-				System.out.print(" ");
-			sleep();
+			try
+			{
+				final List<Comment> commentsFromIssue = service.getComments(repo, issue.getNumber());
 
+				if(commentsFromIssue == null)
+					issues.put(issue, new ArrayList<Comment>(0));
+				else
+					issues.put(issue, commentsFromIssue);
+
+				final int remainingRequests = getClient().getRemainingRequests();
+
+				System.out.print(".");
+				if(iterations % 10 == 0)
+					System.out.print(" ");
+			}
+			catch(RequestException exception)
+			{
+				System.out.println(exception.getMessage());
+			}
+			finally
+			{
+				sleep();
+			}
 		}
-		
+
 		return issues;
 	}
 
