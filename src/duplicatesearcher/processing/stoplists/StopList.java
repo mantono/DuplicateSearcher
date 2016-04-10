7ecf37b9ea7cbf9a070ce4ae19dd710b0f12ef07
Anton Österberg <anton.osterberg@gmail.com>
2016-04-10 23:33:47 +0200

    Created class StopList, that takes a file containing a list of stop words and uses it to remove words from a set of tokens.

---
+++ src/duplicatesearcher/processing/stoplists/StopList.java
@@ -0,0 +1,48 @@
+package duplicatesearcher.processing.stoplists;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import duplicatesearcher.Token;
+import duplicatesearcher.processing.Tokenizer;
+
+public class StopList
+{
+	private final Set<Token> stopWords = new HashSet<Token>();
+
+	public StopList(final File stopListFile) throws IOException
+	{
+		readFileContent(stopListFile);
+	}
+
+	private void readFileContent(File stopListFile) throws IOException
+	{
+		final Path path = Paths.get(stopListFile.toURI());
+		List<String> listLines = Files.readAllLines(path);
+		for(String line : listLines)
+		{
+			final Tokenizer tokenizer = new Tokenizer(line);
+			for(Token token : tokenizer.getTokens())
+				stopWords.add(token);
+		}
+	}
+
+	/**
+	 * Removes all tokens that are present in the stop word list from the given input set.
+	 * @param input the {@link Set} that will from which stop words will be removed.
+	 * @return the amount of words that were removed.
+	 */
+	public int removeStopWords(Set<Token> input)
+	{
+		final int sizeBefore = input.size();
+		input.removeAll(stopWords);
+		return sizeBefore - input.size();
+	}
+
+}
