56c62e50bc7303e2e7ec59b4afa9c0d84919270e
Anton Österberg <anton.osterberg@gmail.com>
2016-04-13 02:23:41 +0200

    Removed redundant imports.

--- src/research/experiment/datacollectiontools/DatasetFileManager.java
+++ src/research/experiment/datacollectiontools/DatasetFileManager.java
@@ -2,12 +2,10 @@ package research.experiment.datacollectiontools;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
