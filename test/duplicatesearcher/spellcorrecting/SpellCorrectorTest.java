d6dcc6a90ddc8bd32ccbf8d01ecbaee4847adaf7
Anton Österberg <anton.osterberg@gmail.com>
2016-04-14 14:53:20 +0200

    Fixed merge conflict.

--- test/duplicatesearcher/spellcorrecting/SpellCorrectorTest.java
+++ test/duplicatesearcher/spellcorrecting/SpellCorrectorTest.java
@@ -13,78 +13,62 @@ import org.junit.Test;
 import duplicatesearcher.Token;
 import duplicatesearcher.processing.spellcorrecting.SpellCorrector;
 
-public class SpellCorrectorTest {
+public class SpellCorrectorTest
+{
 	private SpellCorrector sc;
-	
+
 	@Before
-	public void setUp() throws IOException{
-			sc = new SpellCorrector(new File("dictionary/dict.txt"), 2);
+	public void setUp() throws IOException
+	{
+		sc = new SpellCorrector(new File("dictionary/dict.txt"), 2);
 	}
-	
+
 	@Test
-	public void correctSimpleWordTest(){
+	public void correctSimpleWordTest()
+	{
 		Token test = new Token("hejs");
 		test = sc.correctWord(test);
 		assertEquals(new Token("hej"), test);
-	}	
-	
+	}
+
 	@Test
-	public void correctAboveThresholdWordTest(){
+	public void correctAboveThresholdWordTest()
+	{
 		Token test = new Token("hejsanhejsan");
 		test = sc.correctWord(test);
 		assertEquals(new Token("hejsanhejsan"), test);
-	}	
-	
-	@Test
-	public void correctWordListTest(){
-<<<<<<< HEAD
-		List<Token> list = new ArrayList<Token>();
-		List<Token> listTest = new ArrayList<Token>();
-		
-		list.add(new Token("hejs")); //hej
-		list.add(new Token("slanka")); //slank
-		list.add(new Token("gurkansa")); //oförändrad
-		list.add(new Token("gurkan")); //gurka
-		list.add(new Token("regeringen"));//oförändrad
-		list.add(new Token("issueprocessor"));//oförändrad
-		
-		list = sc.correctWords(list);
-		
-		listTest.add(new Token("hej"));
-		listTest.add(new Token("slank"));
-		listTest.add(new Token("gurkansa"));
-		listTest.add(new Token("gurka"));
-		listTest.add(new Token("regeringen"));
-		listTest.add(new Token("issueprocessor"));
-=======
-		List<String> list = new ArrayList<String>();
-		List<String> expected = new ArrayList<String>();
-		
-		list.add("hejs"); //hej
-		list.add("slanka"); //slank
-		list.add("gurkansa"); //ofÃ¶rÃ¤ndrad
-		list.add("gurkan"); //gurka
-		list.add("regeringen");//ofÃ¶rÃ¤ndrad
-		list.add("issueprocessor");//ofÃ¶rÃ¤ndrad
-		
-		list = sc.correctWords(list);
-		
-		expected.add("hej");
-		expected.add("slank");
-		expected.add("gurkansa");
-		expected.add("gurka");
-		expected.add("regeringen");
-		expected.add("issueprocessor");
->>>>>>> origin/master
-		
-		assertEquals(expected, list);
-		
 	}
-	
+
 	@Test
-	public void tokenTest(){
+	public void correctWordListTest()
+	{
+		List<Token> list = new ArrayList<Token>();
+		List<Token> expected = new ArrayList<Token>();
+
+		list.add(new Token("hejs")); // hej
+		list.add(new Token("slanka")); // slank
+		list.add(new Token("gurkansa")); // ofï¿½rï¿½ndrad
+		list.add(new Token("gurkan")); // gurka
+		list.add(new Token("regeringen"));// ofï¿½rï¿½ndrad
+		list.add(new Token("issueprocessor"));// ofï¿½rï¿½ndrad
+
+		list = sc.correctWords(list);
+
+		expected.add(new Token("hej"));
+		expected.add(new Token("slank"));
+		expected.add(new Token("gurkansa"));
+		expected.add(new Token("gurka"));
+		expected.add(new Token("regeringen"));
+		expected.add(new Token("issueprocessor"));
+
+		assertEquals(expected, list);
+
+	}
+
+	@Test
+	public void tokenTest()
+	{
 		assertEquals(new Token("a"), new Token("a"));
 	}
-	
-	
+
 }
