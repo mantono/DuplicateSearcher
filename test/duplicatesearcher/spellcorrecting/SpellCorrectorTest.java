a5670cc44cb755c105f07905e0f56dbd10b15c2b
Anton <anton.s.friedmann@gmail.com>
2016-04-14 15:43:45 +0200

    Changed constructor
    Changed the constrctor to take both threshold and file as parameters.
    TestCases gives NullPointerException.

--- test/duplicatesearcher/spellcorrecting/SpellCorrectorTest.java
+++ test/duplicatesearcher/spellcorrecting/SpellCorrectorTest.java
@@ -2,59 +2,67 @@ package duplicatesearcher.spellcorrecting;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import duplicatesearcher.Token;
 import duplicatesearcher.processing.spellcorrecting.SpellCorrector;
 
 public class SpellCorrectorTest {
 	private SpellCorrector sc;
 	
 	@Before
-	public void setUp(){
-		sc = new SpellCorrector();
+	public void setUp() throws IOException{
+			sc = new SpellCorrector(new File("dictionary/dict.txt"), 2);
 	}
 	
 	@Test
 	public void correctSimpleWordTest(){
-		String test = "hejs";
+		Token test = new Token("hejs");
 		test = sc.correctWord(test);
-		assertEquals("hej", test);
+		assertEquals(new Token("hej"), test);
 	}	
 	
 	@Test
 	public void correctAboveThresholdWordTest(){
-		String test = "hejsanhejsan";
+		Token test = new Token("hejsanhejsan");
 		test = sc.correctWord(test);
-		assertEquals("hejsanhejsan", test);
+		assertEquals(new Token("hejsanhejsan"), test);
 	}	
 	
 	@Test
 	public void correctWordListTest(){
-		List<String> list = new ArrayList<String>();
-		List<String> listTest = new ArrayList<String>();
+		List<Token> list = new ArrayList<Token>();
+		List<Token> listTest = new ArrayList<Token>();
 		
-		list.add("hejs"); //hej
-		list.add("slanka"); //slank
-		list.add("gurkansa"); //oförändrad
-		list.add("gurkan"); //gurka
-		list.add("regeringen");//oförändrad
-		list.add("issueprocessor");//oförändrad
+		list.add(new Token("hejs")); //hej
+		list.add(new Token("slanka")); //slank
+		list.add(new Token("gurkansa")); //oförändrad
+		list.add(new Token("gurkan")); //gurka
+		list.add(new Token("regeringen"));//oförändrad
+		list.add(new Token("issueprocessor"));//oförändrad
 		
 		list = sc.correctWords(list);
 		
-		listTest.add("hej");
-		listTest.add("slank");
-		listTest.add("gurkansa");
-		listTest.add("gurka");
-		listTest.add("regeringen");
-		listTest.add("issueprocessor");
+		listTest.add(new Token("hej"));
+		listTest.add(new Token("slank"));
+		listTest.add(new Token("gurkansa"));
+		listTest.add(new Token("gurka"));
+		listTest.add(new Token("regeringen"));
+		listTest.add(new Token("issueprocessor"));
 		
 		assertEquals(list, listTest);
 		
+	}
+	
+	@Test
+	public void tokenTest(){
+		assertEquals(new Token("a"), new Token("a"));
 	}
 	
 	
