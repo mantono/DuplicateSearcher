79832ccc2b7a2346f64cfd6e03d8d8421f3a5dfd
Anton Österberg <anton.osterberg@gmail.com>
2016-04-14 14:09:17 +0200

    Added snowball to class path for run shell script.

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -classpath lib/gson-2.2.2.jar:lib/org.eclipse.egit.github.core-2.1.5.jar:bin/ duplicatesearcher.DuplicateSearcher $1 $2
+java -classpath lib/gson-2.2.2.jar:lib/org.eclipse.egit.github.core-2.1.5.jar:lib/snowball-stemmer-1.3.0.581.1.jar:bin/ duplicatesearcher.DuplicateSearcher $1 $2
