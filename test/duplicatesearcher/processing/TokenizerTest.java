1618ab964e2c1e4d102a4ee31c8424124b6ce1e8
Anton Österberg <anton.osterberg@gmail.com>
2016-04-19 00:48:10 +0200

    Removed System.out debugging in test case.

--- test/duplicatesearcher/processing/TokenizerTest.java
+++ test/duplicatesearcher/processing/TokenizerTest.java
@@ -33,10 +33,7 @@ public class TokenizerTest
 		final Token[] expected = new Token[]{new Token("well"), new Token("then"), new Token("this"),
 				new Token("could"), new Token("certainly"), new Token("prove"), new Token("to"), new Token("be"),
 				new Token("a"), new Token("challenge")};
-		
-		for(Token t : output)
-			System.out.println(t);
-		
+
 		assertArrayEquals(expected, output);
 	}
 
@@ -51,64 +48,64 @@ public class TokenizerTest
 
 		assertEquals(expected, output);
 	}
-	
+
 	@Test
 	public void testRemoveApostrophe()
 	{
 		final String input = "We're and we'll, because it's our destiny. Shouldn't it be?";
 		final String expected = "We are and we will, because it our destiny. Shouldnt it be?";
-		
+
 		Tokenizer tokenizer = new Tokenizer("");
 		final String output = tokenizer.removeApostrophes(input);
-		
+
 		assertEquals(expected, output);
 	}
-	
+
 	@Test
 	public void testRemoveURLsWithHttpsUrl()
 	{
 		final String input = "Here is a link https://github.com/mantono/DuplicateSearcher that we want to be removed.";
 		final String expected = "Here is a link  that we want to be removed.";
-		
+
 		Tokenizer tokenizer = new Tokenizer("");
 		final String output = tokenizer.removeURLs(input);
-		
+
 		assertEquals(expected, output);
 	}
-	
+
 	@Test
 	public void testRemoveURLsWithHttpUrl()
 	{
 		final String input = "Here is a link http://github.com/mantono/DuplicateSearcher that we want to be removed.";
 		final String expected = "Here is a link  that we want to be removed.";
-		
+
 		Tokenizer tokenizer = new Tokenizer("");
 		final String output = tokenizer.removeURLs(input);
-		
+
 		assertEquals(expected, output);
 	}
-	
+
 	@Test
 	public void testRemoveURLsWithUrlAndDashes()
 	{
 		final String input = "Here is a link github.com/mantono/DuplicateSearcher that we want to be removed.";
 		final String expected = "Here is a link  that we want to be removed.";
-		
+
 		Tokenizer tokenizer = new Tokenizer("");
 		final String output = tokenizer.removeURLs(input);
-		
+
 		assertEquals(expected, output);
 	}
-	
+
 	@Test
 	public void testRemoveURLsWithDomainName()
 	{
 		final String input = "Here is a link github.com that we want to be removed.";
 		final String expected = "Here is a link  that we want to be removed.";
-		
+
 		Tokenizer tokenizer = new Tokenizer("");
 		final String output = tokenizer.removeURLs(input);
-		
+
 		assertEquals(expected, output);
 	}
 
@@ -158,7 +155,7 @@ public class TokenizerTest
 		assertFalse(Tokenizer.isToken("BAD"));
 		assertFalse(Tokenizer.isToken(null));
 	}
-	
+
 	@Test
 	public void testRemoveEmojis()
 	{
@@ -168,7 +165,7 @@ public class TokenizerTest
 		final String output = tokenizer.removeEmojis(input);
 		assertEquals(expected, output);
 	}
-	
+
 	@Test
 	public void testRemoveNumbers()
 	{
@@ -176,37 +173,37 @@ public class TokenizerTest
 		final String regularIntegers = "1 2 3";
 		String result = tokenizer.removeNumbers(regularIntegers);
 		assertEquals("  ", result);
-		
+
 		final String decimals = "2.0 3.11";
 		result = tokenizer.removeNumbers(decimals);
 		assertEquals(" ", result);
-		
+
 		final String commaDecimals = "2,0 3,11";
 		result = tokenizer.removeNumbers(commaDecimals);
 		assertEquals(" ", result);
-		
+
 		final String commaDelimiteer = "2,300,000 4,111";
 		result = tokenizer.removeNumbers(commaDecimals);
 		assertEquals(" ", result);
-		
+
 		final String dotDelimiteer = "2.300.000 4.111";
 		result = tokenizer.removeNumbers(commaDecimals);
 		assertEquals(" ", result);
-		
+
 		final String variables = "v14 token2";
 		result = tokenizer.removeNumbers(variables);
 		assertEquals("v14 token2", result);
 	}
-	
+
 	@Test
 	public void testRemoveUsername()
 	{
 		Tokenizer tokenizer = new Tokenizer("");
-		
+
 		final String userMentions = "@a-asdas-asd-asd @eu1289 @s-s @mantono";
 		String result = tokenizer.removeUsernames(userMentions);
 		assertEquals("   ", result);
-		
+
 		final String notUsernames = "my.email@adress.com @-notValidUsername @notvalid- ";
 		result = tokenizer.removeUsernames(notUsernames);
 		assertEquals("my.email@adress.com @-notValidUsername @notvalid- ", result);
