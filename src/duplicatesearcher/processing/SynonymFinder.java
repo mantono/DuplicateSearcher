f3a5eb6691a4b75c64ed89dd765ac8ce7a140758
Anton Österberg <anton.osterberg@gmail.com>
2016-04-21 17:46:24 +0200

    Cleaned up in SynonymFinder/SynonymFinderTest.

--- src/duplicatesearcher/processing/SynonymFinder.java
+++ src/duplicatesearcher/processing/SynonymFinder.java
@@ -2,8 +2,6 @@ package duplicatesearcher.processing;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
@@ -11,14 +9,11 @@ import java.util.SortedSet;
 import java.util.TreeSet;
 
 import duplicatesearcher.Token;
-import edu.mit.jwi.Dictionary;
-import edu.mit.jwi.IDictionary;
 import edu.mit.jwi.IRAMDictionary;
 import edu.mit.jwi.RAMDictionary;
 import edu.mit.jwi.data.ILoadPolicy;
 import edu.mit.jwi.item.IIndexWord;
 import edu.mit.jwi.item.ISynset;
-import edu.mit.jwi.item.ISynsetID;
 import edu.mit.jwi.item.IWord;
 import edu.mit.jwi.item.IWordID;
 import edu.mit.jwi.item.POS;
@@ -29,34 +24,12 @@ public class SynonymFinder implements TokenProcessor
 
 	public SynonymFinder() throws IOException, InterruptedException
 	{
-		// construct the URL to the Wordnet dictionary directory
-		String wnhome = System.getenv(" WNHOME ");
-		String path = wnhome + File.separator + " dict ";
-
-		// construct the dictionary object and open it
+		// TODO fix relative path
 		this.dict = new RAMDictionary(new File("/a/oberon-home1/h13/anos3557/workspace/DuplicateSearcher/lib/WordNet-3.0/dict"), ILoadPolicy.NO_LOAD);
 		dict.open();
 		dict.load(true);
 	}
-
-	public void testDict() throws IOException
-	{
-		// construct the URL to the Wordnet dictionary directory
-		//String wnhome = System.getenv("WNHOME");
-		//String path = wnhome + File.separator + "dict";
-		//URL url = new URL("file", null, path);
-		// construct the dictionary object and open it
-		//IDictionary dict = new Dictionary(url);
-		dict.open();
-		// look up first sense of the word " dog "
-		IIndexWord idxWord = dict.getIndexWord(" dog ", POS.NOUN);
-		IWordID wordID = idxWord.getWordIDs().get(0);
-		IWord word = dict.getWord(wordID);
-		System.out.println(" Id = " + wordID);
-		System.out.println(" Lemma = " + word.getLemma());
-		System.out.println(" Gloss = " + word.getSynset().getGloss());
-	}
-
+	
 	public Collection<String> getSynonym(String input)
 	{
 		IIndexWord idxWord = dict.getIndexWord(input, POS.NOUN);
@@ -104,7 +77,14 @@ public class SynonymFinder implements TokenProcessor
 		if(synonyms.isEmpty())
 			return token;
 		
-		return new Token(synonyms.first().toLowerCase());
+		final Token synonym = mostCommonWord(synonyms);
+		
+		return synonym;
+	}
+
+	private Token mostCommonWord(SortedSet<String> synonyms)
+	{
+		return new Token(synonyms.first());
 	}
 
 }
--- test/duplicatesearcher/processing/SynonymFinderTest.java
+++ test/duplicatesearcher/processing/SynonymFinderTest.java
@@ -12,23 +12,11 @@ import duplicatesearcher.Token;
 
 public class SynonymFinderTest
 {
-
-	@Ignore
-	@Test
-	public void testGetSynonym() throws IOException, InterruptedException
-	{
-		SynonymFinder s = new SynonymFinder();
-		s.testDict();
-		Collection<String> syns = s.getSynonym("phone");
-		System.out.println(syns);
-	}
-
 	@Test
 	public void testProcess() throws IOException, InterruptedException
 	{
 		SynonymFinder s = new SynonymFinder();
-		final Token synonym = s.process(new Token("rigor"));
+		final Token synonym = s.process(new Token("dog"));
 		System.out.println(synonym);
 	}
-
 }
