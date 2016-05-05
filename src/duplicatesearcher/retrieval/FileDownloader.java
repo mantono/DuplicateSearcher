2f10990bb9f88c591177e85017e89aef9c721aca
Anton Österberg <anton.osterberg@gmail.com>
2016-05-05 17:44:34 +0200

    Removed redundant imports.

--- src/duplicatesearcher/retrieval/FileDownloader.java
+++ src/duplicatesearcher/retrieval/FileDownloader.java
@@ -2,17 +2,11 @@ package duplicatesearcher.retrieval;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.net.URI;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
@@ -24,14 +18,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.egit.github.core.RepositoryId;
-import org.eclipse.egit.github.core.client.GitHubClient;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.sun.org.apache.xml.internal.security.utils.resolver.ResourceResolverException;
-
-import research.experiment.datacollectiontools.ApiClient;
 
 public class FileDownloader
 {
