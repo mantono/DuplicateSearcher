fa97d7bb72e13908c28976e1e20716c05bb4b22b
Anton Österberg <anton.osterberg@gmail.com>
2016-05-17 17:06:18 +0200

    Regular main method is (temporarily?) deprecated to avoid accidental invocation.

--- src/duplicatesearcher/DuplicateSearcher.java
+++ src/duplicatesearcher/DuplicateSearcher.java
@@ -126,6 +126,7 @@ public class DuplicateSearcher
 		report.buildFile();
 	}
 
+	@Deprecated
 	public static void main(String[] args) throws ClassNotFoundException, IOException, InterruptedException, URISyntaxException
 	{
 
